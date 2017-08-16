8b97e27c1dc1b8959e11d3a2b80885a24080079f
Zonglin-Li6565 <development.my6565@gmail.com>
2017-08-16 00:12:02 -0500

    fixed a include config path issue

--- src/Config.cpp
+++ src/Config.cpp
@@ -9,21 +9,27 @@ Config::Config(const std::string& fileName)
     std::fstream fstrm(fileName);
     json_ = json::value::parse(fstrm);
 
-    if (json_.has_field("INCLUDE"))
+    if (json_.has_field("include"))
     {
         int split = fileName.find_last_of("/\\");
         std::string incFilePath;
         if (split != -1 /*nop*/)
         {
-            incFilePath = fileName.substr(split + 1) + json_["INCLUDE"].
+            incFilePath = fileName.substr(0, split + 1) + json_["include"].
                                                        as_string();
         }
         else
         {
-            incFilePath = json_["INCLUDE"].as_string();
+            incFilePath = json_["include"].as_string();
         }
+
         std::fstream incFstrm(incFilePath);
         include_ = json::value::parse(incFstrm);
-        json_.erase("INCLUDE");
+        json_.erase("include");
     }
+}
+
+bool Config::hasKey(const std::string& key)
+{
+    return json_.has_field(key) || include_.has_field(key);
 }
